26d4a94026c819f451fd12d533fd74ae7c10ccd6
hetken <sometokens@gmail.com>
2025-05-19 00:01:48 +0300

    added annotation @Step

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -3,6 +3,7 @@ package pages;
 import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
 import components.Components;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.attribute;
 import static com.codeborne.selenide.Condition.visible;
@@ -13,64 +14,76 @@ import static com.codeborne.selenide.Selenide.closeWindow;
 
 public class MainPage {
 
+    @Step("Выбрать в выпадающем списке пункт отправления")
     public MainPage inputStartingLocation(String city) {
         $("#direction-from").click();
         $(".rzd-direction-from").$(byText(city)).click();
         return this;
     }
 
+    @Step("Выбрать в выпадающем списке пункт назначения")
     public MainPage inputDestinationLocation(String city) {
         $("#direction-to").click();
         $(".rzd-direction-to").$(byText(city)).click();
         return this;
     }
 
+    @Step("Выбрать в дейтпикере дату отъезда")
     public MainPage chooseDepartureDateFromDatepicker() {
         Components.chooseDepartureDateFromDatepicker();
         return this;
     }
 
+    @Step("Выбрать в дейтпикере дату обратного рейса")
     public MainPage chooseReturnDateFromDatepicker() {
         Components.chooseReturnDateFromDatepicker();
         return this;
     }
 
+    @Step("Кликнуть на кнопку 'Найти'")
     public MainPage searchForConnection() {
         $(byTagAndText("a", "Найти")).click();
         return this;
     }
 
+    @Step("Кликнуть на кнопку сброса поля направления")
     public MainPage resetDirectionField(SelenideElement selector) {
         selector.click();
         return this;
     }
 
+    @Step("Проверить, что поле стало пустым")
     public MainPage verifyCorrectReset(SelenideElement selector) {
         selector.shouldBe(attribute("nodeid", ""));
         return this;
     }
 
+    @Step("Кликнуть на кнопку смены направлений")
     public MainPage clickSwapButton() {
         $(".rzd-swap-directions-button").click();
         return this;
     }
 
+    @Step("Проверить, что значение из поля 'Куда' подставилось в поле 'Откуда'")
     public MainPage verifyDirectionFromFieldValue(String value) {
         $("#direction-from").shouldBe(attribute("expresscode", value));
         return this;
     }
 
+    @Step("Проверить, что значение из поля 'Откуда' подставилось в поле 'Куда'")
     public MainPage verifyDirectionToFieldValue(String value) {
         $("#direction-to").shouldBe(attribute("expresscode", value));
         return this;
     }
 
+    @Step("Проверить адрес страницы ticket.rzd.ru")
     public MainPage verifyCurrentUrl() {
         WebDriverRunner.url().contains("ticket.rzd.ru");
         closeWindow();
         return this;
     }
 
+    @Step("Проверить текст тултипа")
     public MainPage verifyWarningToolTip() {
         $(byText("Введите пункт отправления")).shouldBe(visible);
         return this;
